5245e8769d5945017271fc22e56368a6d8aec21e
Facetozvers <whatthefapmc@gmail.com>
2020-09-23 20:34:03 +0700

    progress

---
+++ resources/views/home.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Synthesis Homes | Rumah Nyaman untuk Keluarga Anda</title>
+</head>
+<body>
+    <header class="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <img class="logo" src="/img/Logo.jpg" alt="">
+    </nav>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active" style="background-image: url('/img/web-slider/WEB-BANNER-SH-AUG-2020-1.jpg')"></div>
+            <div class="carousel-item" style="background-image: url('/img/web-slider/web_slider_greenpark.jpg')">        </div>
+            <div class="carousel-item" style="background-image: url('/img/web-slider/web_slider_edupark.jpg')"></div>
+            <div class="carousel-item" style="background-image: url('/img/web-slider/web_slider_sportpark.jpg')"></div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    </header>
+
+    <div class="container pl-0 pr-0">
+        <div class="row mt-4">
+            <div class="col-lg-6">
+                <h4 class="pt-4"><strong>KENYAMANAN HIDUP<br> DI LINGKUNGAN HIJAU</strong></h4>
+                <p class=" desc-homes pt-4">Proyek hunian terbaru karya Synthesis Development yang dikembangkan di area Ciputat, Tangerang Selatan.  Synthesis Homes dengan luas area 4,7 hektar terdiri dari 3 tipe rumah dengan jumlah total 267 unit dengan mengusung fitur Smart Home. Selain itu, kawasan ini akan dilengkapi 4 taman tematik.
+                    Synthesis Homes, berlokasi 2 km dari Stasiun MRT Lebak Bulus, yang merupakan jalur penghubung utama dengan kawasan sentra bisnis Sudirman dan sekitarnya.
+                    Dengan fasilitas keamanan yang disediakan, penghuni kawasan Synthesis Homes akan merasa lebih aman dalam beraktifitas keseharian.
+                </p>
+            </div>
+            <div class="col-lg-6">
+                <img class="homes4" src="/img/homes4.jpg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional javascript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
