6bbf7bd6f7b97f293417c04f56130f3a5e2084c5
Facetozvers <whatthefapmc@gmail.com>
2020-09-24 22:12:31 +0700

    progress 50%

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <title>Synthesis Homes | Rumah Nyaman untuk Keluarga Anda</title>
 </head>
@@ -14,10 +15,10 @@
     </nav>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active" style="background-image: url('/img/web-slider/WEB-BANNER-SH-AUG-2020-1.jpg')"></div>
-            <div class="carousel-item" style="background-image: url('/img/web-slider/web_slider_greenpark.jpg')">        </div>
-            <div class="carousel-item" style="background-image: url('/img/web-slider/web_slider_edupark.jpg')"></div>
-            <div class="carousel-item" style="background-image: url('/img/web-slider/web_slider_sportpark.jpg')"></div>
+            <div class="carousel-item web-slider active" style="background-image: url('/img/web-slider/WEB-BANNER-SH-AUG-2020-1.jpg')"></div>
+            <div class="carousel-item web-slider" style="background-image: url('/img/web-slider/web_slider_greenpark.jpg')">        </div>
+            <div class="carousel-item web-slider" style="background-image: url('/img/web-slider/web_slider_edupark.jpg')"></div>
+            <div class="carousel-item web-slider" style="background-image: url('/img/web-slider/web_slider_sportpark.jpg')"></div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -30,18 +31,70 @@
     </div>
     </header>
 
-    <div class="container pl-0 pr-0">
+    <div class="container">
         <div class="row mt-4">
-            <div class="col-lg-6">
+            <div class="col-lg-6 col-12">
                 <h4 class="pt-4"><strong>KENYAMANAN HIDUP<br> DI LINGKUNGAN HIJAU</strong></h4>
                 <p class=" desc-homes pt-4">Proyek hunian terbaru karya Synthesis Development yang dikembangkan di area Ciputat, Tangerang Selatan.  Synthesis Homes dengan luas area 4,7 hektar terdiri dari 3 tipe rumah dengan jumlah total 267 unit dengan mengusung fitur Smart Home. Selain itu, kawasan ini akan dilengkapi 4 taman tematik.
                     Synthesis Homes, berlokasi 2 km dari Stasiun MRT Lebak Bulus, yang merupakan jalur penghubung utama dengan kawasan sentra bisnis Sudirman dan sekitarnya.
                     Dengan fasilitas keamanan yang disediakan, penghuni kawasan Synthesis Homes akan merasa lebih aman dalam beraktifitas keseharian.
                 </p>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-6 col-12">
                 <img class="homes4" src="/img/homes4.jpg" alt="">
             </div>
+        </div>
+    </div>
+    <div class="container-fluid pl-0 pr-0 pt-4">
+        <div class="tematik-section pb-5" style="background: rgb(246, 246, 246)">
+        <h5 class="pt-3" style="text-align:center"><strong>4 TAMAN TEMATIK</strong></h5><hr>
+        <div class="row justify-content-center ml-0 mr-0 pt-4">
+            <div id="carouselTematik" class="carousel slide w-75" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselTematik" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselTematik" data-slide-to="1"></li>
+                    <li data-target="#carouselTematik" data-slide-to="2"></li>
+                    <li data-target="#carouselTematik" data-slide-to="3"></li>
+                    <li data-target="#carouselTematik" data-slide-to="4"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item tematik-item active">
+                    <img class="d-block w-100" src="/img/tematik/clubhouse.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item tematik-item">
+                    <img class="d-block w-100" src="/img/tematik/funpark.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item tematik-item">
+                    <img class="d-block w-100" src="/img/tematik/edupark.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item tematik-item">
+                    <img class="d-block w-100" src="/img/tematik/sportpark.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item tematik-item">
+                    <img class="d-block w-100" src="/img/tematik/greenpark.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev tematik-control" href="#carouselTematik" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next tematik-control" href="#carouselTematik" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="container-fluid" style="background: url('/img/Bag.jpg')">
+        <h4 class="pt-4" style="text-align:center; color: white">Untuk informasi hubungi<br> Contact Person kami</h4>
+        <div class="row justify-content-center pt-3 pb-4">
+            <a class="btn btn-primary pt-2 pb-2 button-cp" style="background: white; border:1px solid grey"><i class="fa fa-phone-square" aria-hidden="false"></i><span class="pl-3">Lihat Contact Person</span></a>
+        </div>
+    </div>
+    <div class="container-fluid pt-4 pb-4" style="background: rgb(47,61,46)">
+        <div class="row justify-content-center">
+            <img class="map-pic" src="/img/Newmaps-synthesishomes.jpg" alt="">
         </div>
     </div>
 
